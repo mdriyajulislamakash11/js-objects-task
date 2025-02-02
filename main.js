f562ce33fd24c494dbc96f8dc3ad98ede9e0d285
mdriyajulislamakash11 <hafejmohammadakash@gmail.com>
2025-02-02 23:35:27 +0600

    done here now haha

--- main.js
+++ main.js
@@ -34,16 +34,16 @@ console.log(car)
 // Task-3
 // Display the physics marks as output.
 
-// const student = {
-//     name: "Hamim Sakep",
-//     id: 5421,
-//     physics: {
-//         subject: "HSC Physics",
-//         author: "Shahjahan Tapan",
-//         marks: 30
-//     }
-// };
-// console.log(student.physics.marks)
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+console.log(student.physics.marks)
 
 
 
